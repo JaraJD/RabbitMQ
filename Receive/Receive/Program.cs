e87b6646f170989d5d82a2e3a92d704deb8901d0
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-10-03 20:12:52 -0500

    Despliegue inicial

---
+++ Receive/Receive/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+
+            // Directo
+            channel.ExchangeDeclare("direct_exchange", ExchangeType.Direct, true);
+            var queueNameDirect = channel.QueueDeclare("queue.direct", true, false, false).QueueName;
+            channel.QueueBind(queueNameDirect, "direct_exchange", "direct_routing_key");
+
+            var consumerDirect = new EventingBasicConsumer(channel);
+            consumerDirect.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Recibido en Direct: '{message}'");
+            };
+
+            channel.BasicConsume(queue: queueNameDirect,
+                                 autoAck: true,
+                                 consumer: consumerDirect);
+
+            // Tipo topic
+            channel.ExchangeDeclare("topic_exchange", ExchangeType.Topic, true);
+            var queueNameTopic1 = channel.QueueDeclare().QueueName;
+            var queueNameTopic2 = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queueNameTopic1, "topic_exchange", "topic.routing.*");
+            channel.QueueBind(queueNameTopic2, "topic_exchange", "*.routing.key");
+
+            var consumerTopic1 = new EventingBasicConsumer(channel);
+            consumerTopic1.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Recibido en Topic 1: '{message}'");
+            };
+
+            var consumerTopic2 = new EventingBasicConsumer(channel);
+            consumerTopic2.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Recibido en Topic 2: '{message}'");
+            };
+
+            channel.BasicConsume(queue: queueNameTopic1,
+                                 autoAck: true,
+                                 consumer: consumerTopic1);
+
+            channel.BasicConsume(queue: queueNameTopic2,
+                                 autoAck: true,
+                                 consumer: consumerTopic2);
+
+            // Tipo fanout
+            channel.ExchangeDeclare("fanout_exchange", ExchangeType.Fanout, true);
+            var queueNameFanout1 = channel.QueueDeclare().QueueName;
+            var queueNameFanout2 = channel.QueueDeclare().QueueName;
+            var queueNameFanout3 = channel.QueueDeclare().QueueName;
+
+            channel.QueueBind(queueNameFanout1, "fanout_exchange", "");
+            channel.QueueBind(queueNameFanout2, "fanout_exchange", "");
+            channel.QueueBind(queueNameFanout3, "fanout_exchange", "");
+
+            var consumerFanout1 = new EventingBasicConsumer(channel);
+            consumerFanout1.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Recibido en Fanout 1: '{message}'");
+            };
+
+            var consumerFanout2 = new EventingBasicConsumer(channel);
+            consumerFanout2.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Recibido en Fanout 2: '{message}'");
+            };
+
+            var consumerFanout3 = new EventingBasicConsumer(channel);
+            consumerFanout3.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Recibido en Fanout 3: '{message}'");
+            };
+
+            channel.BasicConsume(queue: queueNameFanout1,
+                                 autoAck: true,
+                                 consumer: consumerFanout1);
+
+            channel.BasicConsume(queue: queueNameFanout2,
+                                 autoAck: true,
+                                 consumer: consumerFanout2);
+
+            channel.BasicConsume(queue: queueNameFanout3,
+                                 autoAck: true,
+                                 consumer: consumerFanout3);
+
+            Console.WriteLine("Presiona cualquier tecla para salir.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Send/Send/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost",
+            Port = 5672,
+            UserName = "guest",
+            Password = "guest"
+        };
+
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            // Exchange directo
+            channel.ExchangeDeclare("direct_exchange", ExchangeType.Direct, true);
+            //channel.QueueDeclare("queue.direct", true, false ,false);
+            //channel.QueueBind("queue.direct", "direct_exchange", "direct_routing_key");
+            var message = "Mensaje para el exchange directo";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish("direct_exchange", "direct_routing_key", null, body);
+            Console.WriteLine($"Enviado: '{message}'");
+
+            // Exchange topic
+            channel.ExchangeDeclare("topic_exchange", ExchangeType.Topic, true);
+            //channel.QueueDeclare("queue.topic1", true, false, false);
+            //channel.QueueDeclare("queue.topic2", true, false, false);
+            //channel.QueueBind("queue.topic1", "topic_exchange", "topic.routing.key1");
+            //channel.QueueBind("queue.topic2", "topic_exchange", "topic.routing.queue.key2");
+            var message1 = "Mensaje para el exchange de tipo topic1";
+            var message2 = "Mensaje para el exchange de tipo topic2";
+            var message3 = "Mensaje para ambas colas que estan conectadas al exchange de tipo topic";
+            var body1 = Encoding.UTF8.GetBytes(message1);
+            var body2 = Encoding.UTF8.GetBytes(message2);
+            var body3 = Encoding.UTF8.GetBytes(message3);
+            channel.BasicPublish("topic_exchange", "topic.routing.key1", null, body1);
+            channel.BasicPublish("topic_exchange", "topic.routing.queue.key2", null, body2);
+            channel.BasicPublish("topic_exchange", "topic.routing.*", null, body3);
+            Console.WriteLine($"Enviado: '{message1}'");
+            Console.WriteLine($"Enviado: '{message2}'");
+            Console.WriteLine($"Enviado: '{message3}'");
+
+            // Exchange fanout
+            //channel.ExchangeDeclare("fanout_exchange", ExchangeType.Fanout, true);
+            ////channel.QueueDeclare("queue.fanout1", true, false, false);
+            ////channel.QueueDeclare("queue.fanout2", true, false, false);
+            ////channel.QueueBind("queue.fanout1", "fanout_exchange", "");
+            ////channel.QueueBind("queue.fanout2", "fanout_exchange", "");
+            //var message4 = "Mensaje para el exchange de tipo fanout";
+            //var body4 = Encoding.UTF8.GetBytes(message4);
+            //channel.BasicPublish("fanout_exchange", "", null, body4);
+            //Console.WriteLine($"Enviado: '{message4}'");
+        }
+    }
+}
